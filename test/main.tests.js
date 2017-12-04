a5a763f4ee0960000e90b6356e408ba21ba401da
Patrick Biffle <pbiffle@gmail.com>
2017-12-04 09:43:36 -0700

    fixed math problem

--- test/main.tests.js
+++ test/main.tests.js
@@ -5,7 +5,7 @@ const assert = require('assert')
 describe("#toTheFuture", function (){
   it("should square all numbers input and add them together", function(){
     assert.deepEqual(code.toTheFuture(4,6,8), 116)
-    assert.deepEqual(code.toTheFuture(1,2,3), 13)
+    assert.deepEqual(code.toTheFuture(1,2,3), 14)
     assert.deepEqual(code.toTheFuture(3,4,5), 50)
     }
   )}
